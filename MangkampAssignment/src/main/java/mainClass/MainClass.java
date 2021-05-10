8dd10c9f5a2760fcd6d20272c18d0d89be118482
pauham <paula.hammer.gottskar@gmail.com>
2021-05-10 15:57:37 +0200

    Unstable

--- MangkampAssignment/src/main/java/mainClass/MainClass.java
+++ MangkampAssignment/src/main/java/mainClass/MainClass.java
@@ -32,30 +32,7 @@ public class MainClass {
 
 			Events event = new Events();
 			
-			if (gender == 1) {
-				System.out.println("Track");
-				for (int i = 0; i < numParticipants; i++) {
-					System.out.print("Insert result in seconds for " + newParticipants[i].getName() + ": ");
-					double resultEvent = scan.nextDouble();
-					counter.fourHundredMD(resultEvent);
-					System.out.println(counter.getResult());
-					resultEvent = scan.nextDouble();
-					
-					System.out.print("Insert result in seconds for " + newParticipants[i].getName() + ": ");
-					counter.longJumpD(resultEvent);
-					System.out.println(counter.getResult());} 
-				
-			} else if (gender == 2) {
-				System.out.println("Field");
-				for (int i = 0; i < numParticipants; i++) {
-					System.out.print("Insert result in centimeters for " + newParticipants[i].getName() + ": ");
-					double resultEvent = scan.nextDouble();
-					counter.javelinThrowH(resultEvent);
-					System.out.println(counter.getResult());
-				}
-			} else {
-				System.out.println("Please, choose 1 or 2!");
-			}
+			
 		}
 	}
 }
