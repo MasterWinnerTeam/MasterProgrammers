2793f805ba67a9d64bf3d4a3be5a21dcef4999cb
pauham <paula.hammer.gottskar@gmail.com>
2021-05-10 15:57:24 +0200

    Update: Unstable

--- MangkampAssignment/src/main/java/mainClass/MainClass.java
+++ MangkampAssignment/src/main/java/mainClass/MainClass.java
@@ -3,51 +3,59 @@ package mainClass;
 import java.util.Scanner;
 
 public class MainClass {
-	//TEST 2021-04-30
+	// TEST 2021-04-30
 
 	public static void main(String[] args) {
-	//	String[][] eventVariables = {{"25", "24","1"}, {
-		
+		// String[][] eventVariables = {{"25", "24","1"}, {
+
 		System.out.println("Welcome to the olympic games!");
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("How many participants? ");
 		int numParticipants = scan.nextInt();
 		Participants[] newParticipants = new Participants[numParticipants];
-		
+
 		System.out.println("Example: Pär Gren SWE");
 		for (int i = 0; i < numParticipants; i++) {
 			System.out.print("Name of participant " + (i + 1) + " and nationality: ");
-			
+
 			String name = scan.next() + scan.nextLine();
 			newParticipants[i] = new Participants(name);
-			}
-			for(int i = 0; i < numParticipants; i++) {
-			System.out.println((i + 1) + " " + newParticipants [i].getName());
-			}
-			{
-				ResultCounter counter = new ResultCounter();
-				int gender = 1;
-				
-				if (gender == 1) {
+		}
+		for (int i = 0; i < numParticipants; i++) {
+			System.out.println((i + 1) + " " + newParticipants[i].getName());
+		}
+		{
+			ResultCounter counter = new ResultCounter();
+			System.out.println("Please press 1 for woman and 2 for man");
+			int gender = scan.nextInt();
+
+			Events event = new Events();
+			
+			if (gender == 1) {
 				System.out.println("Track");
 				for (int i = 0; i < numParticipants; i++) {
-				System.out.print("Insert result in seconds for " +newParticipants[i].getName() + ": ");
-				double resultEvent = scan.nextDouble();
-				counter.fourHundredMD(resultEvent);
-				System.out.println(counter.getResult());
-				}
-			} else {
+					System.out.print("Insert result in seconds for " + newParticipants[i].getName() + ": ");
+					double resultEvent = scan.nextDouble();
+					counter.fourHundredMD(resultEvent);
+					System.out.println(counter.getResult());
+					resultEvent = scan.nextDouble();
+					
+					System.out.print("Insert result in seconds for " + newParticipants[i].getName() + ": ");
+					counter.longJumpD(resultEvent);
+					System.out.println(counter.getResult());} 
+				
+			} else if (gender == 2) {
 				System.out.println("Field");
 				for (int i = 0; i < numParticipants; i++) {
-				System.out.print("Insert result in centimeters for " +newParticipants[i].getName() + ": ");
-				double resultEvent = scan.nextDouble();
-				counter.javelinThrowH(resultEvent);
-				System.out.println(counter.getResult());
+					System.out.print("Insert result in centimeters for " + newParticipants[i].getName() + ": ");
+					double resultEvent = scan.nextDouble();
+					counter.javelinThrowH(resultEvent);
+					System.out.println(counter.getResult());
 				}
+			} else {
+				System.out.println("Please, choose 1 or 2!");
 			}
 		}
 	}
 }
-
-
